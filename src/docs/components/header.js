d8a63ba35924190a55e38e5ba67fa012e7c98810
eng-ziad fawzy <ziadbusiness2000@gmail.com>
2025-11-23 14:31:18 -0800

    when the display arrives inot 700 pixel show the navbar

--- src/docs/components/header.js
+++ src/docs/components/header.js
@@ -2,10 +2,34 @@ import Logo from "./logo";
 import NavBar from "./navbar";
 import '../style/components/header.css'
 import Search from "./search";
-import { useEffect, useState } from "react";
+import { StrictMode, useEffect, useState } from "react";
 
 export default function Header () {
     const [Yaxis, setYaxis] = useState(0);
+    const [displayWidth,setDisplayWidth] = useState(0)
+    const [navIcon,setNavIcon] = useState(false)
+    
+
+    useEffect(() => {
+        const size = () =>{
+            setDisplayWidth(window.innerWidth);
+        } 
+        window.addEventListener("resize", size);
+        
+        return () => {
+            window.removeEventListener(size);
+        }
+    },[])
+
+    useEffect(() => {
+        if(displayWidth < 700) {
+            setNavIcon(true);
+        }
+        else {
+            setNavIcon(false);
+        }
+    },[displayWidth])
+
     useEffect(() => {
         const handleScrollY = () => {
             setYaxis(Math.floor(window.scrollY));
@@ -16,11 +40,22 @@ export default function Header () {
             window.removeEventListener("scroll", handleScrollY)
         };
     }, [])
+    
     return (
         <header className={Yaxis > 100 ? "sticky-header" : "static-header"}>
             <Logo />
-            <NavBar/>
-            <Search/>
+            {navIcon ? 
+            <div className="nav">
+                <div className="line li-1"></div>
+                <div className="line li-2"></div>
+                <div className="line li-3"></div>
+            </div>
+            :
+            <StrictMode>
+                <NavBar/>
+                <Search/>
+            </StrictMode>
+            }
         </header>
     )
 }
