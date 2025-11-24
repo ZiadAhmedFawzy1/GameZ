ae94afa3032a233982f3afa8dec8b7a4567ec935
eng-ziad fawzy <ziadbusiness2000@gmail.com>
2025-11-24 23:45:45 +0200

    handle dimention of cards

--- src/docs/pages/main.js
+++ src/docs/pages/main.js
@@ -1,12 +1,11 @@
 import '../style/pages/main.css'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import Card from "../components/card";
-import Data from '../api/data.json';
 import { useContext, useEffect, useState } from "react";
 import { Auth } from "../context/context";
 import Details from "../components/details";
@@ -14,11 +13,36 @@ import AboutUs from "../components/about_us";
 import Store from "../components/store";
 import VerifyBox from '../components/VerifyBox';
 export default function Main() {
-    const {clips, data, loading, verify} = useContext(Auth);
+    const {clips, data, loading, verify, displayWidth} = useContext(Auth);
+    const [previewSlide, setPreviewSlide] = useState(null)
     const [idImg,setIdImg] = useState(1);
+    
+    // to transfer between photos
     useEffect(() => {
         setIdImg(data[0]?.id);
     }, [loading]);
+    
+
+    useEffect(() => {
+    const getPreview = async () => {
+        // const preview = await displayWidth > 809 ?  setPreviewSlide(3) : setPreviewSlide(2);
+        // return preview;
+        if(displayWidth >= 809) {
+            setPreviewSlide(3)
+        }
+        else if (displayWidth < 809 && displayWidth >= 550) {
+            setPreviewSlide(2)
+        }
+        else if(displayWidth < 550 && displayWidth >= 460) {
+            setPreviewSlide(3)
+        }
+        else {
+            setPreviewSlide(2)
+        }
+    }
+    getPreview();
+    },[displayWidth, previewSlide])
+
     return (
         <div>
             {verify.verifyBox &&
@@ -53,11 +77,12 @@ export default function Main() {
                 <h3>recommended & best</h3>
                 <div className="container">
                     <Swiper
-                modules={[Navigation, Pagination]}
+                modules={[Pagination]}
                 navigation
                 loop={true}
                 spaceBetween={35}
-                slidesPerView={3}
+                // to decide how many slide display on monitor depending on width of browser
+                slidesPerView={previewSlide}
                 className="mySwiper"
             >
                 {data.map((e,i) => 
